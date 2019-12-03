36290b543d597da30b7e3e7501ef77f662dbc3ad
Iván Munguía <ivanalejandro249@gmail.com>
2019-12-03 12:07:01 -0600

    chore: Setup credo lintern

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule AdventOfCode2019.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -21,8 +22,13 @@ defmodule AdventOfCode2019.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: "credo --strict"
     ]
   end
 end
